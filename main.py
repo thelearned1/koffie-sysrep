a6f4c8ac2da58d5faa31f8d20674ab662885f199
Ned Watson <watsonle@iastate.edu>
2022-09-25 14:33:00 -0400

    Add validation for input file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from array import array
+import string
 import pandas as pd
 import numpy as np
 import datetime as dt
+from dateutil.relativedelta import relativedelta
+import re
 
-INPUT_FILE_PATH = 'input_data.xslx'
+INPUT_FILE_PATH = 'input_data.xlsx'
 
 def read_file() -> pd.DataFrame:
+    print ('reading '+INPUT_FILE_PATH)
     df = pd.read_excel(INPUT_FILE_PATH)
     return df
-
 
 def get_tax_rate(state):
     state_rates = {'IL': .0251, 'TN': .01766, }
@@ -32,8 +36,7 @@ def get_annual_days (start_date: dt.date) -> int:
 
         
     '''
-    one_year = dt.timedelta(years=1)  
-    end_date = start_date+one_year
+    end_date = start_date+relativedelta(years=1)
     return (end_date-start_date).days
 
 def get_pro_rata_gwp (srs: pd.Series) -> pd.Series:
@@ -51,8 +54,9 @@ def get_daily_gwp (srs: pd.Series) -> pd.Series:
     return srs['Annual GWP']/get_annual_days(srs['Effective Date'])
 
 def get_earned_unearned_premium (srs: pd.Series) -> pd.Series:
-    rd = dt.date.today()
-    sd = srs['Effective Date'], ed = srs['Expiration Date']
+    rd = dt.datetime.today().date()
+    sd = srs['Effective Date']
+    ed = srs['Expiration Date']
     if (rd <= sd):
         effective_so_far = 0
         ineffective_remaining = srs['Effective Days']
@@ -69,13 +73,51 @@ def get_earned_unearned_premium (srs: pd.Series) -> pd.Series:
 def get_taxes (srs: pd.Series) -> pd.Series:
     return get_tax_rate(srs['State'])*srs['Pro-Rata GWP']
 
-def sanitize_data (df: pd.DataFrame): return df # STUB
+def dt_cleanup (d):
+    if (isinstance(d, dt.datetime)):
+        return d.date()
+    elif (isinstance(d, dt.date)):
+        return d
+    else:
+        try:
+            nd = dt.datetime.strptime(d.replace('O', '0'), '%Y-%m-%d').date()
+            return nd
+        except:
+            return np.NaN
+
+def int_cleanup (i):
+    if (not isinstance(i, (int, float))):
+        try:
+            i = int(i.replace('O', '0'))
+        except:
+            return np.NaN
+    return i
+
+def sanitize_row (srs: pd.Series) -> array: 
+    vinre = '^[0-9a-zA-Z]{17}$'
+    vin = srs['VIN']
+    vin = vin if (vin and len(vin)==17 and re.search(vinre, vin)!=None) else np.NaN
+    effective = dt_cleanup(srs['Effective Date'])
+    expiration = dt_cleanup(srs['Expiration Date'])
+    if (effective > expiration): 
+        effective = np.NaN
+    gwp = int_cleanup(srs['Annual GWP'])
+    return [vin, effective, expiration, gwp] 
+
+def sanitize_df (df: pd.DataFrame):
+    ndf = df.copy()
+    ndf[['VIN', 'Effective Date', 
+         'Expiration Date', 
+         'Annual GWP']] = ndf.apply(sanitize_row, axis=1, result_type='expand')
+    return ndf.dropna(subset=['VIN', 'Effective Date', 'Expiration Date', 'Annual GWP'])
 
 def main():
-    report_date = '2022-08-01'
-    df = sanitize_data(read_file())
+    s_report_date = '2022-08-01'
+    # my processing follows
+    report_date = dt.datetime.strptime(s_report_date, '%Y-%m-%d').date()
+    df = sanitize_df(read_file())
     df['Effective Days'] = df.apply(lambda srs: (srs['Expiration Date']-
-                                                 srs['End Date'].days), 
+                                                 srs['Effective Date']).days, 
                                     axis=1)
     df['Daily GWP'] = df.apply(get_daily_gwp, axis=1)
     df['Pro-Rata GWP'] = df.apply(get_pro_rata_gwp, axis=1)
@@ -84,6 +126,9 @@ def main():
                                         axis=1,
                                         result_type='expand')
     df['Taxes'] = df.apply(get_taxes, axis=1)
-    # your processing precedes
-    write_new_file(df, report_date)
+    # my processing precedes
+    write_new_file(df, s_report_date)
     return
+
+if (__name__=='__main__'):
+    main()
